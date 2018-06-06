1206b8f555c51cd1e3735438fc895d102d321f9e
Bro <38715190+Wirevel@users.noreply.github.com>
2018-06-06 14:52:21 +0300

    update 0.5

---
+++ app.js
@@ -0,0 +1,14 @@
+const userScore = 0;
+const compScore = 0;
+const userScore_span = document.getElementById("player");
+const compScore_span = document.getElementById("comp");
+const tulosTaulu_div = document.querySelector(".tulosTaulu");
+const tulos_div = document.querySelector(".tulos");
+const rock_div = document.getElementById("rock");
+const paper_div= document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+rock_div.addEventListener('click', function() {
+    console.log("hey you clicked on rock");
+})
+console.log("wut");
